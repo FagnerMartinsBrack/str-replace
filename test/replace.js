53ad104a7a8ac2e5efe298e1044113b242d002ef
Fagner Brack <github@fagnermartins.com>
2015-12-15 15:52:29 -0200

    Create a proper identation for the tests

--- test/replace.js
+++ test/replace.js
@@ -2,13 +2,18 @@ var replace = require( "../src/str-replace" );
 
 module.exports = {
   replace_all: function( assert ) {
-    var actual = replace.all( "/" ).from( "/home/dir" ).to( "\\" );
+    var actual = replace
+      .all( "/" )
+      .from( "/home/dir" )
+      .to( "\\" );
     var expected = "\\home\\dir";
     assert.strictEqual( actual, expected );
     assert.done();
   },
   replace_all_ignoring_case: function( assert ) {
-    var actual = replace.all( "G" ).ignoreCase()
+    var actual = replace
+      .all( "G" )
+        .ignoreCase()
       .from( "Eggs" )
       .to( "f" );
     var expected = "Effs";
