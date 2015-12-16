75c7e4fc07aa9bc59b4eff7a578e7909fd55d0d0
Fagner Brack <github@fagnermartins.com>
2015-12-16 00:36:30 -0200

    Identation is 2 spaces not 4

--- src/str-replace.js
+++ src/str-replace.js
@@ -7,23 +7,23 @@ var replaceAll = function( oldToken ) {
           var _token;
           var index = -1;
           if ( configs.ignoringCase ) {
-              _token = oldToken.toLowerCase();
-              while((
-                  index = string
-                    .toLowerCase()
-                    .indexOf(
-                      _token,
-                      index >= 0 ?
-                        index + newToken.length : 0
-                      )
-                    ) !== -1
-              ) {
-                  string = string
-                    .substring( 0, index ) +
-                      newToken +
-                    string.substring( index + newToken.length );
-              }
-              return string;
+            _token = oldToken.toLowerCase();
+            while((
+              index = string
+                .toLowerCase()
+                .indexOf(
+                  _token,
+                  index >= 0 ?
+                    index + newToken.length : 0
+                  )
+                ) !== -1
+            ) {
+              string = string
+                .substring( 0, index ) +
+                  newToken +
+                string.substring( index + newToken.length );
+            }
+            return string;
           }
           return string.split( oldToken ).join( newToken );
         }
