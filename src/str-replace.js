ccaed5a04306a6bdf94279a38638de66c3bf188d
Fagner Brack <github@fagnermartins.com>
2015-12-19 10:10:33 -0200

    Refactor the replace so the types are consistent with the docs

--- src/str-replace.js
+++ src/str-replace.js
@@ -1,51 +1,54 @@
-var replaceAll = function( occurrences ) {
-  var configs = this;
+var CreateReplaceDefinition = function( replaceAlgorithm ) {
+  var ReplaceDefinition = function( occurrences ) {
+    var definitionContext = this;
+    return {
+      ignoringCase: function() {
+        return ReplaceDefinition.call({
+          ignoringCase: true
+        }, occurrences );
+      },
+      from: function( target ) {
+        return ReplaceOperation(function executeReplace( replacement ) {
+          return replaceAlgorithm.call( definitionContext, occurrences, replacement, target );
+        });
+      }
+    };
+  };
+  return ReplaceDefinition;
+};
+
+var ReplaceOperation = function( replaceExecution ) {
   return {
-    from: function( target ) {
-      return {
-        to: function( replacement ) {
-          var template;
-          var index = -1;
-          if ( configs.ignoringCase ) {
-            template = occurrences.toLowerCase();
-            while((
-              index = target
-                .toLowerCase()
-                .indexOf(
-                  template,
-                  index === -1 ? 0 : index + replacement.length
-                )
-              ) !== -1 ) {
-              target = target
-                .substring( 0, index ) +
-                  replacement +
-                target.substring( index + replacement.length );
-            }
-            return target;
-          }
-          return target.split( occurrences ).join( replacement );
-        }
-      };
-    },
-    ignoringCase: function() {
-      return replaceAll.call({
-        ignoringCase: true
-      }, occurrences );
-    }
+    to: replaceExecution
   };
 };
 
-var replace = function( occurrences ) {
-  return {
-    from: function( target ) {
-      return {
-        to: function( replacement ) {
-          return target.replace( occurrences, replacement );
-        }
-      };
+var replaceAll = CreateReplaceDefinition(function( occurrences, replacement, target ) {
+  var template;
+  var index = -1;
+  if ( this.ignoringCase ) {
+    template = occurrences.toLowerCase();
+    while((
+      index = target
+        .toLowerCase()
+        .indexOf(
+          template,
+          index === -1 ? 0 : index + replacement.length
+        )
+      ) !== -1 ) {
+      target = target
+        .substring( 0, index ) +
+          replacement +
+        target.substring( index + replacement.length );
     }
-  };
-};
+    return target;
+  }
+  return target.split( occurrences ).join( replacement );
+});
+
+var replace = CreateReplaceDefinition(function( occurrences, replacement, target ) {
+  return target.replace( occurrences, replacement );
+});
 
 replace.all = replaceAll;
 
