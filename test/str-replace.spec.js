10c219a6b2571da8d97f857901edd6e7506d4a7d
Fagner Brack <github@fagnermartins.com>
2016-03-28 13:18:18 +1100

    Remove useless spacing in the tests

--- test/str-replace.spec.js
+++ test/str-replace.spec.js
@@ -2,27 +2,21 @@ var replace = require("../str-replace");
 var expect = require("expect.js");
 
 describe("Replace first", function() {
-
   it("should replace first occurrences", function() {
     expect(replace("a").from("aa").with("e")).to.be("ea");
   });
-
   it("should replace first occurrences ignoring the case", function() {
     expect(replace("a").ignoringCase().from("AA").with("e")).to.be("eA");
   });
-
 });
 
 describe("Replace all", function() {
-
   it("should replace all occurrences", function() {
     expect(replace.all("/").from("/home/dir").with("\\")).to.be("\\home\\dir");
   });
-
   it("should replace all occurrences ignoring the case", function() {
     var replaceAllHouseIgnoringCase = replace.all("house").ignoringCase();
     expect(replaceAllHouseIgnoringCase.from("Many Houses").with("Horse")).to.be("Many Horses");
     expect(replace.all("house").from("Many Houses").with("Horse")).to.be("Many Houses");
   });
-
 });
