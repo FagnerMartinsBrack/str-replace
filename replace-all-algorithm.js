72fc6bab113ac583807b2fb277eb66e5c088560b
Fagner Brack <github@fagnermartins.com>
2016-03-28 12:55:08 +1100

    Partially apply the algorithms for external reusability

--- replace-all-algorithm.js
+++ replace-all-algorithm.js
@@ -1,10 +1,11 @@
 var escapeRegexSpecialChars = require("escape-string-regexp");
+var partial = require("partial-application").partial;
 
-module.exports = function(occurrences, replacement, inputTarget) {
+module.exports = partial(function(occurrences, replacement, inputTarget) {
   var flags = ["g"];
   if (this.ignoringCase) {
     flags.push("i");
   }
   var regex = new RegExp(escapeRegexSpecialChars(occurrences), flags.join(""));
   return inputTarget.replace(regex, replacement);
-};
+});
